48519ea4d42ef79c1ef202af2e17daf286d365fb
matkatmusic <matkatmusic@gmail.com>
2020-04-18 11:32:43 -0700

    added part7 instructions

--- main.cpp
+++ main.cpp
@@ -1,55 +1,85 @@
+
+#include <iostream>
 /*
-Project 4 - Part 6 / 9
-Video: Chapter 5 Part 3
- 
-Create a branch named Part6
- 
- Lambdas
- 
-    Do not delete your previous main. you will be adding to it.
+Project 4: Part 7 / 9
+Video: Chapter 5 Part 4
+
+Create a branch named Part7
+
+Do not delete your previous main. you will be adding to it.
+
+Templates and Containers
 
     Build/Run often with this task to make sure you're not breaking the code with each step.
     I recommend committing after you get each step working so you can revert to a working version easily if needed.
- 
- 1) add two member functions named "apply()" to each of your Heap-Allocated Numeric Type wrappers.
-         both apply() functions should work with chaining
- 
- 2) One of the apply() functions should takes a std::function<> object as the function argument.
-    the std::function<> object should return *this;
- 
- 3) the other apply() function should take a function pointer. 
-    the function pointer should return void.
- 
- 4) Make both of the member functions's Callable Function Parameter use your owned object as it's single parameter.
-         e.g. if you manage your owned object via std::unique_ptr<T>, you'd use this for your std::function argument:
-             std::function< OwnedT&(std::unique_ptr<T>&)>
-             
-         if you managed your owned object via a raw pointer, you'd use this for your std::function argument:
-             std::function<OwnedT&(T&)>    
- 
- 5) call that Callable Function Parameter in the apply() member function.
-         be sure to practice safe std::function usage (make sure it's not a nullptr function being called)
- 
- 6) Using one instance of each of your Heap-Allocated Numeric Type wrappers: Call your apply() member function twice in main()
-         a) once with a lambda as the argument
-         b) once with a free function as the argument.
-         c) there will be a total of 6 calls to apply() in main(), 2x per HeapType, IntType, DoubleType instance.
-         If this confuses you, rewatch the video where I pass a free function in to a function that wants a function pointer
- 
- 8) Make your lambda & free function update the value of your held object
- 
- 9) use std::cout statements to print out the results of calling apply()
- 
-build/run to make sure you don't have any errors
+    it's very easy to mess this task up. 
 
+#1) if you're not using std::unique_ptr to own your heap-allocated type as a member variable, 
+    replace your manual memory management techniques with a private std::unique_ptr member variable.
+
+#2) replace your Heap-Allocated Numeric Type-owning class with a templated class called Numeric.
+        replace all of your previous instances of your separate classes with this templated class.
+
+#3) add a 'using <some name> = <your class template parameter>;' 
+        treat this type declaration via 'using' as a static member variable
+        use this Type alias as the argument everywhere you previously used the template argument.
+        this will make it very easy to change the type if needed.
+            i.e. if you have 'std::unique_ptr<NumericType> value;' before
+                you'd replace NumericType in that variable declaration with '<some name>'
+        
+#4) you'll need to pair it with decltype() to help the compiler figure out the type of the object 
+    your class owns when you make your lambda and free function that takes your unique_ptr.  
+    i.e. like this for determining the template parameter of the unique_ptr function argument to your class's apply() function
+        std::unique_ptr< decltype( <instance of class> )::<some name> >
+    
+#5) template your free function for the apply() that takes a function pointer so it can be used with your Wrapper class's apply() function
+
+#6) add an explicit template specialization for 'double' of your wrapper class
+        this template specialization will have one apply() function instead of 2. 
+
+#7) this apply() function will be templated, and expect a Callable object, not std::function<>. 
+        the function should allow for chaining.  
+        the callable object should return void, like the function pointer-based apply() function in the primary class template
+
+#8) instantiate your explicit template specialization
+
+#9) call the apply function twice, once with a lambda and once with the free function
+        the free function is templated, so you might need to call it including the template arguments.
+        
+#10) in addition to using the lambda argument to modify the owned object:  (your previous task's lambdas did this same thing) 
+        make the lambda use your explicit template instance (maybe via a std::cout), 
+
+#11) now that your class is templated, you'll need to adjust your logic in your division function to handle if your input is a zero or not, based on your templated type.  
+        - look up how to use std::is_same<>::value on cppreference to determine the type of your template parameter.
+        
+        - look up how to use std::numeric_limits<>::epsilon() to determine if you're dividing by a floating point 0
+        
+        - read about Knuth's algorithm here: https://www.learncpp.com/cpp-tutorial/relational-operators-and-floating-point-comparisons/
+
+        - in plain-english, you'll need to implement logic like this:
+        if your template type is an int
+                if your parameter's type is also an int
+                        if your parameter is 0
+                                don't do the division
+                else if it's less than epsilon
+                        dont do the divison
+        else if it's less than epsilon
+                warn about doing the division
+
+        - to make these checks work during compilation, your if() statements will need to be 'constexpr':  if constexpr (expression)
+
+compile/link/run to make sure you don't have any errors
+
+Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
+ 
 Make a pull request after you make your first commit and pin the pull request link to our DM thread.
 
 send me a DM to check your pull request
 
  Wait for my code review.
- 
- If you need to see an example, look at https://bitbucket.org/MatkatMusic/pfmcpptasks/src/master/Projects/Project4/Part6Example.cpp
- */
+
+If you need to view an example, see: https://bitbucket.org/MatkatMusic/pfmcpptasks/src/master/Projects/Project4/Part7Example.cpp
+*/
 
 
 
